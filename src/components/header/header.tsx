a5bf1c2ecdbe6729b56e3adfa3a314e28517c047
AWCostabile <jupiter.adept@gmail.com>
2021-09-05 21:54:44 +1000

    Updates

--- src/components/header/header.tsx
+++
@@ -1,8 +0,0 @@
-import { PageSection } from "components/page-section";
-import React from "react";
-
-export const Header: React.FC = ({ children }) => (
-  <PageSection background="#28C" color="#F2F2F2" padding={{ all: 16 }}>
-    {children}
-  </PageSection>
-);
--- src/styles/utils/helpers.ts
+++
@@ -1,9 +0,0 @@
-import { createSideStyle } from "./style-side";
-
-export const marginStyles = createSideStyle<
-  "margin" | "marginTop" | "marginLeft" | "marginRight" | "marginBottom"
->("margin");
-
-export const paddingStyles = createSideStyle<
-  "padding" | "paddingTop" | "paddingLeft" | "paddingRight" | "paddingBottom"
->("padding");
