5d32fcd4cd4dcaad828cded27bc43871073c0aad
AWCostabile <jupiter.adept@gmail.com>
2021-09-13 18:06:33 +1000

    refactor: add Google analytics

--- src/common/languages/english.tsx
+++ src/common/languages/english.tsx
@@ -88,6 +88,7 @@ export const english: ILanguage = {
 
   info: englishInfo,
 
+
   // ==========================================================================
   //  CREDITS -> Replace the name below with your name
   // ==========================================================================
